726bb8078bca17bd76324f6b2d4da46365996d35
sssukho <sssukho@fasoo.com>
2022-08-31 11:25:38 +0900

    프로젝트 세팅 및 테스트 완료

--- jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -4,12 +4,17 @@ import jpabook.jpashop.model.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+//@Rollback(false) // 트랜잭션 어노테이션 통해서 롤백 처리된 것을 방지한다 (롤백이 되지 않고 DB에 반영이 된다)
 class MemberRepositoryTest {
+
     @Autowired
     MemberRepository memberRepository;
 
@@ -26,6 +31,7 @@ class MemberRepositoryTest {
         // then
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
     }
 
 }
