61c232fdfd790f33e2a1647c2b6b1d7406e7f1de
sssukho <sssukho@gmail.com>
2022-10-23 18:37:32 +0900

    섹션 6. 주문 검색 기능 개발 내용 정리 및 코드 완성

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,9 +1,19 @@
 package jpabook.jpashop.repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,5 +29,94 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) { ... }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // 동적인 쿼리를 만들 수 있는 방법1 - 김영한 강사님은 이 방법은 안쓰신다고 함.
+        return findAllByString(orderSearch);
+        // 동적인 쿼리를 만들 수 잇는 방법 2 - JPA Criteria로 처리 - 이방식도 김영한 강사님은 실무에서 쓰기 너무 어렵다라고 하심
+//        return findAllByCriteria(orderSearch);
+    }
+
+    private List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += "where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); // 최대 1000건
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    private List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        // 주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
+
+//    public List<Order> findAllByQuerydsl(OrderSearch orderSearch) {
+//        QOrder order = QOrder.order;
+//        QMember member = QMember.member;
+//
+//        return query
+//                .select(order)
+//                .from(order)
+//                .join(order.member, member)
+//                .where(statusEq(orderSearch.getOrderStatus()),
+//                        nameLike(orderSearch.getMemberName()))
+//                .limit(1000)
+//                .fetch();
+//    }
+//
+//    private BooleanExpression statusEq(OrderStatus statusCond) {
+//        if (statusCOnd == null) {
+//            return null;
+//        }
+//        return order.staatus.eq(statusCond);
+//    }
 }
--- study/Section 6. 주문 도메인 개발.md
+++ study/Section 6. 주문 도메인 개발.md
@@ -445,3 +445,176 @@ public class OrderService {
 
   - `주문 취소()` : 주문을 취소하면 그만큼 재고가 증가해야 한다.
     - 주문을 취소하려면 먼저 주문을 해야 한다. Given 절에서 주문하고 When 절에서 해당 주문을 취소했다. Then 절에서 주문상태가 주문 취소 상태인지(`CANCEL`), 취소한 만큼 재고가 증가했는지 검증한다.
+
+
+
+### 주문 검색 기능 개발
+
+JPA에서 동적 쿼리를 어떻게 해결해야 하는가?
+
+- 검색 조건 파라미터 `OrderSearch`
+
+  ``` java
+  package jpabook.jpashop.domain;
+  
+  public class OrderSearch {
+    private String memberName; // 회원 이름
+    private OrderStatus orderStatus; // 주문 상태[ORDER, CANCEL]
+    
+    // Getter, Setter
+  }
+  ```
+
+- 검색을 추가한 주문 리포지토리 코드
+
+  ``` java
+  package jpabook.jpashop.repository;
+  
+  @Repository
+  public class OrderRepository {
+    @PersistenceContext
+    EntityManager em;
+    
+    public void save(Order order) {
+      em.persist(order);
+    }
+    
+    public Order findOne(Long id) {
+      return em.find(Order.class, id);
+    }
+    
+    public List<Order> findAll(OrderSearch orderSearch) {
+      // ... 검색 로직
+    }
+  }
+  ```
+
+  - `findAll(OrderSearch orderSearch)` 메서드는 검색 조건에 동적으로 쿼리를 생성해서 주문 엔티티를 조회한다.
+
+
+
+### JPQL로 처리
+
+``` java
+public List<Order> findAllByString(OrderSearch orderSearch) {
+  // language = JPQL
+  String jpql = "select o From Order o join o.member m";
+  boolean isFirstCondition = true;
+  
+  // 주문 상태 검색
+  if (orderSearch.getOrderStatus() != null) {
+    if (isFirstCondition) {
+      jpql += " where";
+      isFirstCondition = flase;
+    } else {
+      jpql += " and";
+    }
+    jpql += " o.status = :status";
+  }
+  
+  // 회원 이름 검색
+  if (StringUtils.hasText(orderSearch.getMemberName())) {
+    if (isFirstCondition) {
+      jpql += "where";
+      isFirstCondition = false;
+    } else {
+      jpql += " and";
+    }
+    jpql += " m.name like :name";
+  }
+  
+  TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); // 최대 1000건
+  
+  if (orderSearch.getOrderStatus() != null) {
+    query = query.setParameter("status", orderSearch.getOrderStatus());
+  }
+  if (StringUtils.hasText(orderSearch.getMemberName())) {
+    query = query.setParameter("name", orderSearch.getMemberName());
+  }
+  
+  return query.getResultList();
+}
+```
+
+- JPQL 쿼리를 문자로 생성하기는 번거롭고, 실수로 인한 버그가 충분히 발생할 수 있다.
+
+
+
+### JPA Criteria로 처리 - 잘 안쓰는 방식이라 그냥 편하게 들으면 됨
+
+``` java
+public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+  
+ 
+  CriteriaBuilder cb = em.getCriteriaBuilder();
+  CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+  Root<Order> o = cq.from(Order.class);
+  Join<Order, Member> m = o.join("member", JoinType.INNER); // 회원과 조인
+  
+  List<Predicate> criteria = new ArrayList<>();
+  
+  // 주문 상태 검색
+  if (orderSearch.getOrderStatus() != null) {
+    Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+    criteria.add(status);
+  }
+  
+  // 회원 이름 검색
+  if (StringUtils.hasText(orderSearch.getMemberName())) {
+    Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+    criteria.add(name);
+  }
+  
+  cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+  TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); // 최대 1000건
+  return query.getResultList();
+}
+```
+
+JPA Criteria는 JPA 표준 스펙이지만 실무에서 사용하기에 너무 복잡하다. 결국 다른 대안이 필요하다. 많은 개발자가 비슷한 고민을 했지만, 가장 멋진 해결책은 Querydsl이 제시했다.
+
+
+
+### Querydsl 로 처리
+
+``` java
+public List<Order> findAllByQuerydsl(OrderSearch orderSearch) {
+  QOrder order = QOrder.order;
+  QMember member = QMember.member;
+        
+  return query
+          .select(order)
+          .from(order)
+          .join(order.member, member)
+          .where(statusEq(orderSearch.getOrderStatus()),
+                 nameLike(orderSearch.getMemberName()))
+          .limit(1000)
+          .fetch();
+}
+
+private BooleanExpression statusEq(OrderStatus statusCond) {
+  if (statusCOnd == null) {
+    return null;
+  }
+  return order.staatus.eq(statusCond);
+}
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
